c2ee917f29f90f42cdfb7f89b65ad2afae05c90d
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-05-22 18:12:40 +0100

    Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJYL3RqqUQ7GjvEaoaF8PkvxilkmFYZAg",
+    authDomain: "netflix-clone-df32d.firebaseapp.com",
+    projectId: "netflix-clone-df32d",
+    storageBucket: "netflix-clone-df32d.appspot.com",
+    messagingSenderId: "436293948215",
+    appId: "1:436293948215:web:eeebc862f3298fa0a000b2"
+  };
