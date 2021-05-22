9285e8bbbe804b502607d4d83a567eb364e329bc
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-05-22 21:10:27 +0100

    - Add Home Screen - Finish home screen nav

---
+++ src/Nav.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt=""
+        />
+
+        <img
+          className="nav__avater"
+          src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
