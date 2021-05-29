d7825409a5b7776c47c2abe73d07b42e4120592a
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-05-29 18:05:25 +0100

    Add row component

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,15 +2,24 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
-
       <Banner />
-
-      {/*Row*/}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />{" "}
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      ​<Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />{" "}
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      ​<Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
