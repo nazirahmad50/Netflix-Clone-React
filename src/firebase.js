5bc09edd541895d72f7b9656711272ec20f5e19a
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-06 17:33:36 +0100

    - Extend firebase config - Add register and login with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBJYL3RqqUQ7GjvEaoaF8PkvxilkmFYZAg",
-    authDomain: "netflix-clone-df32d.firebaseapp.com",
-    projectId: "netflix-clone-df32d",
-    storageBucket: "netflix-clone-df32d.appspot.com",
-    messagingSenderId: "436293948215",
-    appId: "1:436293948215:web:eeebc862f3298fa0a000b2"
-  };
+  apiKey: "AIzaSyBJYL3RqqUQ7GjvEaoaF8PkvxilkmFYZAg",
+  authDomain: "netflix-clone-df32d.firebaseapp.com",
+  projectId: "netflix-clone-df32d",
+  storageBucket: "netflix-clone-df32d.appspot.com",
+  messagingSenderId: "436293948215",
+  appId: "1:436293948215:web:eeebc862f3298fa0a000b2",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
